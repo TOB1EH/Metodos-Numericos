5b97051648837c234af0548c7328d912ef31aa0c
Tobias Funes <tobiasfunes@hotmail.com.ar>
2025-12-03 00:21:03 -0300

    feat(utils): Agregar calculadora de funciones y notas para Windows

---
+++ calculo_de_f.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double f (double x);
+
+int main(int argc, char const *argv[])
+{
+    double x = 0.0;
+    double y = 0.0;
+
+    printf("Ingrese el valor de x: ");
+    scanf("%lf", &x);
+    // y = f(x);
+    printf("El valor de f(%.4f) es: %.4f\n", x, f(x));
+    return 0;
+}
+
+double f (double x)
+{
+    // return (sqrt(1 + pow(x, 2)));
+    // return (x / (sqrt(1 + pow(x, 2))));
+    return (exp(sqrt(1 + x)) * log(1 + ( 2 * (x * x))));
+}
