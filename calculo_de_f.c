352aedf1baf079c83ed4f6bf046c3cac320b2581
Tobias Funes <tobiasfunes@hotmail.com.ar>
2025-12-11 18:42:41 -0300

    refactor(utils): Agregar bucle infinito y actualizar función

--- calculo_de_f.c
+++ calculo_de_f.c
@@ -9,10 +9,16 @@ int main(int argc, char const *argv[])
     double x = 0.0;
     double y = 0.0;
 
-    printf("Ingrese el valor de x: ");
-    scanf("%lf", &x);
-    // y = f(x);
-    printf("El valor de f(%.4f) es: %.4f\n", x, f(x));
+    do
+    {
+        printf("\nIngrese el valor de x: ");
+        scanf("%lf", &x);
+        // y = f(x);
+        printf("El valor de f(%.4f) es: %.4f\n", x, f(x));
+    
+    } while (1);
+    
+    
     return 0;
 }
 
@@ -20,5 +26,8 @@ double f (double x)
 {
     // return (sqrt(1 + pow(x, 2)));
     // return (x / (sqrt(1 + pow(x, 2))));
-    return (exp(sqrt(1 + x)) * log(1 + ( 2 * (x * x))));
+    // return (exp(sqrt(1 + x)) * log(1 + ( 2 * (x * x))));
+
+
+    return (log(1.0 - (x / 6.5)));
 }
